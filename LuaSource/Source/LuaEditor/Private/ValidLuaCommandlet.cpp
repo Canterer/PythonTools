f7f710a6f04092461c31d2a6f4b3a786367854d4
Canterer <1170836331@qq.com>
2022-09-08 10:14:45 +0800

    Update ValidLuaCommandlet.cpp

---
+++ LuaSource/Source/LuaEditor/Private/ValidLuaCommandlet.cpp
@@ -0,0 +1,12 @@
+#include "ValidLuaCommandlet.h"
+
+
+int32 UValidLuaCommandlet::Main(const FString& CmdLineParams)
+{
+	int32 i = 0 ;
+	// test uobjet
+	// test singleton
+	// test ufunction
+	// test
+	return 0;
+}
