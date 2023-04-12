02d0f24afce08e4210d42da37a1444ec6b5a8e5e
Canterer <1170836331@qq.com>
2023-04-12 15:46:20 +0800

    Update AutoM3U8.py

--- ffmpeg/AutoM3U8.py
+++ ffmpeg/AutoM3U8.py
@@ -4,11 +4,11 @@ import os
 import sys
 import getopt
 
-ffmpegExePath = "D:/WorkSpaces/SoftSpaces/ffmpeg/bin/ffmpeg.exe"
+ffmpegExePath = ""#"D:/WorkSpaces/SoftSpaces/ffmpeg/bin/ffmpeg.exe"
 masterName = "rep.m3u8"
-inputRootDir = "C:\\Users\\Administrator\\Desktop\\HLS_Spaces\\input"
-outputRootDir = "C:\\Users\\Administrator\\Desktop\\HLS_Spaces\\output"
-outputSubDir = "222"
+# inputRootDir = "C:\\Users\\Administrator\\Desktop\\HLS_Spaces\\input"
+outputRootDir = ""#"C:\\Users\\Administrator\\Desktop\\HLS_Spaces\\output"
+outputSubDir = "segment"
 outputFileName = "req_%v_.m3u8"
 segment_type_ts = "mpegts"
 segment_type_m4s = "fmp4"
@@ -18,18 +18,26 @@ DefaultMaxRate = 1.5
 DefaultMinRate = 0
 
 def getCmd(inputFileName, outputSubDir, segment_type_flag, RateScale):
-    if "." not in inputFileName:
-        if outputSubDir is None:
-            outputSubDir = inputFileName
+    if inputFileName[-4:] != ".mp4":#仅输入文件名 不带后缀 默认当前目录
         inputFileName = inputFileName + ".mp4"
+    
+    inputFilePath = ""
+    dirname = os.path.dirname(inputFileName)
+    if dirname == "":
+        inputFilePath = os.path.join(os.getcwd(), inputFileName)
+    else:
+        inputFilePath = inputFileName
+    basename = os.path.basename(inputFileName)
+    if outputSubDir is None:
+        outputSubDir = basename.rsplit('.',1)[0]
     if RateScale is None:
         RateScale = 1
-    return runCmd(inputFileName, outputSubDir, segment_type_flag, RateScale*DefaultVideoBite, RateScale*DefaultMaxRate, RateScale*DefaultMinRate)
+    return runCmd(inputFilePath, outputSubDir, segment_type_flag, RateScale*DefaultVideoBite, RateScale*DefaultMaxRate, RateScale*DefaultMinRate)
 
-def runCmd(inputFileName, outputSubDir, segment_type_flag, videoBite=DefaultVideoBite, maxRate=DefaultMaxRate, minRate=DefaultMinRate):
-    print("runCmd args:\n inputFileName:{0} outputSubDir:{1} segment_type_flag:{2} videoBite:{3} maxRate:{4} minRate:{5}".format(inputFileName, outputSubDir, segment_type_flag, videoBite, maxRate, minRate))
+def runCmd(inputFilePath, outputSubDir, segment_type_flag, videoBite=DefaultVideoBite, maxRate=DefaultMaxRate, minRate=DefaultMinRate):
+    print("runCmd args:\n inputFilePath:{0} outputSubDir:{1} segment_type_flag:{2} videoBite:{3} maxRate:{4} minRate:{5}".format(inputFilePath, outputSubDir, segment_type_flag, videoBite, maxRate, minRate))
     VideoBiteStr = str.format(" -b:v {0}M -maxrate:v {1}M -minrate:v {2}M -bufsize:v {3}M ", videoBite, maxRate, minRate, 2*maxRate)
-    cmdStr = ffmpegExePath + " -i " + os.path.join(inputRootDir,inputFileName)
+    cmdStr = ffmpegExePath + " -i " + inputFilePath
     cmdStr = cmdStr + " -map v:0 " + VideoBiteStr
     cmdStr = cmdStr + " -map a:0 -c:a:0 copy"
     hls_segment_type = segment_type_ts if segment_type_flag else segment_type_m4s
@@ -39,33 +47,57 @@ def runCmd(inputFileName, outputSubDir, segment_type_flag, videoBite=DefaultVide
     folderPath = os.path.join(outputRootDir,outputSubDir,hls_segment_type)
     if not os.path.exists(folderPath):
         os.makedirs(folderPath)
+
     cmdStr = cmdStr + os.path.join(folderPath,outputFileName)
-    print("cmd: "+cmdStr)
+    print("cmd: "+cmdStr)    
+    cmdStr = "cd {0} && {1}".format(folderPath, cmdStr)
     return cmdStr
 
 def main(argv):
+    # 
+    global ffmpegExePath,outputRootDir
+    ffmpegExePath = os.path.join(os.getcwd(), "ffmpeg.exe")
+    outputRootDir = os.path.join(os.getcwd(), "output")
+
     inputFileName=outputSubDir=segment_type_flag=rateScale=None
-    opts, args = getopt.getopt(argv,"i:f:r:",["outputSubDir=","segement_type_flag=","rateScale="])
+    inputFileName=argv[0]
+    
+    shortArgsStr = "i:f:r:"
+    longArgsList = ["outputSubDir=","segement_type_flag=","rateScale=","outputRootDir=","ffmpegExePath=","help"]
+    opts, args = getopt.getopt(argv,shortArgsStr,longArgsList)
     for opt, value in opts:
         if opt == "-i":
             inputFileName = value
         elif opt == "--outputSubDir":
             outputSubDir = value
+        elif opt == "--outputRootDir":
+            outputRootDir = value
+        elif opt == "--ffmpegExePath":
+            ffmpegExePath = value
         elif opt in ("-f","--segement_type_flag"):
             segment_type_flag = value
         elif opt in ("-r","--rateScale"):
             rateScale = value
-    if len(args) == 1:
-        inputFileName = args[0]
+        elif opt == "--help":
+            print("shortArgsStr:",shortArgsStr)
+            print("longArgsList:")
+            for arg in longArgsList:
+                print("\t--"+arg)
+            return
+
+    print("argv:",*argv)
+    print("ffmpeg Path:",ffmpegExePath)
+    print("output Path:",outputRootDir)
 
     if inputFileName is None:
         print("inputFileName is need!!!")
+        print(len(args))
         return
 
     if segment_type_flag is None:
-        cmdStr = getCmd(inputFileName,outputSubDir,True,rateScale)
-        os.system(cmdStr)
         cmdStr = getCmd(inputFileName,outputSubDir,False,rateScale)
+        os.system(cmdStr)
+        cmdStr = getCmd(inputFileName,outputSubDir,True,rateScale)
         os.system(cmdStr)
     else:
         cmdStr = getCmd(inputFileName,outputSubDir,segment_type_flag,rateScale)
