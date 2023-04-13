7382de6fc17b12cb404f3fed634a999bff2465aa
Canterer <1170836331@qq.com>
2023-04-13 16:37:01 +0800

    Update LoadM3U8.py

--- LoadM3U8/LoadM3U8.py
+++ LoadM3U8/LoadM3U8.py
@@ -10,6 +10,8 @@ from concurrent.futures import as_completed
 import requests
 from urllib3 import disable_warnings
 disable_warnings()
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
 
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.82 Safari/537.36"
@@ -25,7 +27,7 @@ def reurl(url):
         return True
  
 #获取密钥
-def getKey(keystr,url):
+def getKey(keystr,prefix_url,web_ip_url):
     keyinfo= str(keystr)
     method_pos= keyinfo.find('METHOD')
     comma_pos = keyinfo.find(",")
@@ -34,7 +36,11 @@ def getKey(keystr,url):
     quotation_mark_pos = keyinfo.rfind('"')
     key_url = keyinfo[uri_pos:quotation_mark_pos].split('"')[1]
     if reurl(key_url) == False:
-        key_url = url.rsplit("/", 1)[0] + "/" + key_url
+        if key_url[0] == "/":
+            key_url = web_ip_url + key_url
+        else:
+            key_url = prefix_url + "/" + key_url
+    print("key_url:",key_url)
     res = requests.get(key_url,headers=headers)
     key = res.content
     print("getKey method:",method)
@@ -50,27 +56,32 @@ def getKey(keystr,url):
 #decrypt:是否加密
 #down_path:下载地址
 #key:密钥
-def download(down_url,url,decrypt,down_path,key):
-    if reurl(down_url) == False:
-        filename = down_url
-        down_url = url.rsplit("/", 1)[0] + "/" + down_url
-    else:
-        filename = down_url.rsplit("/", 1)[1]
+def download(ts_info,prefix_url,web_ip_url,decrypt,down_path,key):
+    ts_url = ts_info[0]
+    filename = ts_info[1]
+    down_url = ts_url
+    if reurl(ts_url) == False:
+        if ts_url[0] == "/":
+            down_url = web_ip_url + ts_url
+        else:
+            down_url = prefix_url + "/" + ts_url
     try:
+        print("get ",down_url)
         res = requests.get(down_url, stream=True, verify=False,headers=headers)
     except Exception as e:
         print("error requests.get url:{0} exception:{1}".format(down_url,e))
         return
-    down_ts_path = down_path+"/{0}".format(filename)
+    ts_path = down_path+"/{0}".format(filename)
     if decrypt:
         cryptor =  AES.new(key, AES.MODE_CBC, key)
-    with open(down_ts_path,"wb+") as file:
+    with open(ts_path,"wb+") as file:
         for chunk in res.iter_content(chunk_size=1024):
             if chunk:
                 if decrypt:
                     file.write(cryptor.decrypt(chunk))
                 else:
                     file.write(chunk)
+    return down_url
     
 #合并ts文件
 #dest_file:合成文件名
@@ -82,6 +93,9 @@ def merge_to_mp4(dest_file, source_path,ts_list, delete=False):
     if len(files)!=len(ts_list):
         print("文件不完整！")
         return
+    folderPath = os.path.dirname(dest_file)
+    if not os.path.exists(folderPath):
+        os.makedirs(folderPath)
     with open(dest_file, 'wb') as fw:
         for file in ts_list:
             with open(source_path+"/"+file, 'rb') as fr:
@@ -94,13 +108,28 @@ def main(*args):
     print("args:",*args)
     url = args[0]#"https://index.m3u8"
     delete_ts_flag = False
-    out_file_name = "result.mp4"
+    out_file_name = None
     if len(args) > 1:
         delete_ts_flag = bool(args[1])
-    if len(args) > 1:
+    if len(args) > 2:
         out_file_name = args[2]
     
     print("url:{0} delete_ts_flag:{1}".format(url,delete_ts_flag))
+    web_ip_url = url[:8]+url[8:].split("/",1)[0]
+    prefix_url = url.split("/",1)[0]
+
+    #设置下载路径
+    down_root_path="tmp"
+    sub_folder_index = 1
+    if len(args) > 3:
+        sub_folder_index = int(args[3])
+    wordList = url[8:].split("/")
+    if len(wordList) <= sub_folder_index:
+        print("sub_folder_index need < ",len(wordList))
+        return
+    down_sub_folder=wordList[sub_folder_index]
+
+
     #使用m3u8库获取文件信息
     video = m3u8.load(url)
     #设置是否加密标志
@@ -109,11 +138,10 @@ def main(*args):
     #判断是否加密
     print("video.keys:",video.keys)
     if len(video.keys) > 0 and video.keys[0] is not None:
-        method,key =getKey(video.keys[0],url)
+        method,key =getKey(video.keys[0],prefix_url,web_ip_url)
         decrypt = True
-    #设置下载路径
-    down_root_path="tmp"
-    down_sub_folder=url.rsplit("/", 2)[1]
+    
+    
     #判断是否需要创建文件夹
     if not os.path.exists(down_root_path):
         os.mkdir(down_root_path)
@@ -123,39 +151,40 @@ def main(*args):
         os.mkdir(down_path)
     
     #ts列表
-    ts_list=[]
-    unload_ts_uri_list=[]
+    ts_name_list=[]
+    unload_ts_info_list=[]
     #把ts文件名添加到列表中
     total_ts_num = len(video.segments)
     ts_name="ts_name_none"
     # for i in range(total_ts_num)
     for filename  in video.segments:
-        if reurl(filename.uri):
+        if reurl(filename.uri) or filename.uri[0] == "/":
             ts_name = filename.uri.rsplit("/", 1)[1]
         else:
             ts_name = filename.uri
-        ts_list.append(ts_name)
+        ts_name_list.append(ts_name)
         if not os.path.exists(os.path.join(down_path,ts_name)):
-            unload_ts_uri_list.append(filename.uri)
+            unload_ts_info_list.append([filename.uri,ts_name])
     #开启线程池
     with concurrent.futures.ThreadPoolExecutor() as executor:
         obj_list = []
         # begin = time.time()#记录线程开始时间
-        unload_ts_num = len(unload_ts_uri_list)
+        unload_ts_num = len(unload_ts_info_list)
         print("unload_ts_num:",unload_ts_num)
         for i in range(unload_ts_num):
-            obj = executor.submit(download,unload_ts_uri_list[i],url,decrypt,down_path,key)
+            obj = executor.submit(download,unload_ts_info_list[i],prefix_url,web_ip_url,decrypt,down_path,key)
             obj_list.append(obj)
 		#查看线程池是否结束
         ts_count = 0
         for future in as_completed(obj_list):
             ts_count = ts_count + 1
-            print("{0}/{1}=={2}".format(ts_count,unload_ts_num,ts_count/unload_ts_num*100))
-            # print("{0}/{1}=={2}%%future.status:{3} {4}".format(ts_count,unload_ts_num,ts_count/unload_ts_num*100,future.status(),future))
-    merge_to_mp4(out_file_name, down_path,ts_list,delete_ts_flag)#合并ts文件
-    # times = time.time() - begin #记录线程完成时间
-    # print(times)
+            print("progress {0}%  Current/Total = {1}/{2} \n\t{3}".format(round(ts_count/unload_ts_num*100, 1),ts_count,unload_ts_num,future.result()))
+    merge_to_mp4(out_file_name, down_path, ts_name_list, delete_ts_flag)#合并ts文件
+    # # times = time.time() - begin #记录线程完成时间
+    # # print(times)
 
+def mainTest(*args):
+    print("args:",*args)
 
 if __name__ == "__main__":
     main(*sys.argv[1:])
