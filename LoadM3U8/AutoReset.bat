7e7d9b9abec85ec4fa3a2fb2f963e85d1fcfc7bf
Canterer <1170836331@qq.com>
2023-04-14 11:43:40 +0800

    Update AutoReset.bat

--- LoadM3U8/AutoReset.bat
+++ LoadM3U8/AutoReset.bat
@@ -1,16 +1,24 @@
 @echo off
 
-ping -n 600 127.0.0.1
+@REM ping -n 30 127.0.0.1
+echo ┌────────────────────────────────────────────────────────────────────────────────────────────────────────┐
+set /p = ■<nul
+for /L %%i in (1 1 52) do @set /p a=■<nul&ping -n 5 127.0.0.1>nul
+echo 100%%
+echo └────────────────────────────────────────────────────────────────────────────────────────────────────────┘
+
 for /f "tokens=2" %%i in (recordCmdPid.txt) do taskkill /pid %%i
 
-for /f "tokens=* skip=1" %%i in (Input.txt) do (
+for /f "tokens=* skip=1" %%i in (Input.txt) do (	
 	echo %%i>>$
 )
 for /f "tokens=*" %%i in (Input.txt) do (
+	echo "FirstLine:" %%i
 	echo %%i>>$
 	goto out
 )
 :out
 move $ Input.txt
-start cmd /k AutoLoad.bat
-exit
+@REM start cmd /k AutoLoad.bat
+@REM exit
+pause
